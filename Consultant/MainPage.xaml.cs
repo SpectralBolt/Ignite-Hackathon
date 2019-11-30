942058afe8475cbd08e78c4fcc87ae2bb0813d6d
Spectralbolt <37422018+Rattenfengar@users.noreply.github.com>
2019-11-30 01:36:17 -0500

    Agregar archivos de proyecto.

---
+++ Consultant/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace Consultant
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<string> Items { get; set; }
+        public MainPage()
+        {
+            Items = new ObservableCollection<string> ();
+            this.InitializeComponent();
+        }
+
+        private void OutputTextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var text =(TextBlock)sender;
+            
+            if (!string.IsNullOrEmpty(text.SelectedText)){
+                Debug.WriteLine(text.SelectedText);
+                Items.Add(text.SelectedText);
+            }
+        }
+    }
+}
---
+++ Speech2Text_test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace Speech2Text_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecognizeSpeechAsync().Wait();
+            Console.WriteLine("Press any key...");
+            Console.ReadLine();
+        }
+
+
+        public static async Task RecognizeSpeechAsync()
+        {
+            Console.WriteLine("Starting Speech2Text service...");
+            var config = SpeechConfig.FromSubscription("d882cca2d3b44735b0760cbaece4b340", "westus");
+            config.SpeechRecognitionLanguage="es-MX";
+            using (var audioInput = AudioConfig.FromWavFileInput(@"D:\VS Projects\Ignite\Consultant\Speech2Text_test\Speech.wav"))
+            {
+                using ( var recognizer = new SpeechRecognizer(config, audioInput))
+                {
+                    Console.WriteLine("Recognizing first result...");
+                    var result = await recognizer.RecognizeOnceAsync();
+
+                    if (result.Reason == ResultReason.RecognizedSpeech)
+                    {
+                        Console.WriteLine($"We recognized: {result.Text}");
+                    }
+                    else if (result.Reason == ResultReason.NoMatch)
+                    {
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    }
+                    else if (result.Reason == ResultReason.Canceled)
+                    {
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
