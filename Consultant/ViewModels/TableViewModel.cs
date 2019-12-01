d9d8503ef4eb1d16620bf77980391ca5967042f6
Spectralbolt <37422018+Rattenfengar@users.noreply.github.com>
2019-12-01 06:59:05 -0500

    Ok

--- Consultant/ViewModels/TableViewModel.cs
+++ Consultant/ViewModels/TableViewModel.cs
@@ -24,6 +24,24 @@ namespace Consultant.ViewModels
         private async void LoadRecords()
         { 
             var response=await http.GetAsync(new Uri("https://localhost:44371/api/Office"));
+            System.Net.ServicePointManager.ServerCertificateValidationCallback += delegate (
+            object sender,
+            X509Certificate cert,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+                    {
+                        if (sslPolicyErrors == SslPolicyErrors.None)
+                        {
+                            return true;   //Is valid
+                        }
+
+                        if (cert.GetCertHashString() == "2c38449dc9ad99d12eb0b6197f99e60c8706a399")
+                        {
+                            return true;
+                        }
+
+                        return false;
+                    };
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
             var items = JsonConvert.DeserializeObject<List<Office>>(content);
@@ -32,5 +50,6 @@ namespace Consultant.ViewModels
                 Offices.Add(item);
             }
         }
-    }
+
+    }//2c38449dc9ad99d12eb0b6197f99e60c8706a399
 }
